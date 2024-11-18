cb856ff7a0a1d2d5643257d67543001a4a874253
咖啡老師 <coffeeteacher@gmail.com>
2024-11-18 23:37:49 +0800

    todo-2

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,32 @@
-function Todo({todo}) {
+
+import { MdDelete, MdEdit } from "react-icons/md";
+import EditForm from "./EditForm";
+
+// 解構deleteTodo屬性
+function Todo({ todo, deleteTodo, toggleCompleted, toggleIsEdit, editTodo }) {
     return (
-        <div className="todo">
-            {/* <p>繳手機費</p> */}
-            {/* <p>{todo}</p> */}
-            {/* 因為改為陣列物件，所以todo內容為content */}
-            <p>{todo.content}</p>
-        </div>
+        todo.isEdit ? <EditForm todo={todo} editTodo={editTodo} />
+            // 1. 新增一個completed類別控制css變化
+            // 2. 使用反引號搭配三元運算子檢查isCompleted
+            // 如果true => 套用completed類別規則，反之，不套用規則
+            : <div className={`todo ${todo.isCompleted ? 'completed' : ''}`}>               
+
+                {/* 因為改為陣列物件，所以todo內容為content */}
+                <p onClick={() => { toggleCompleted(todo.id) }}>{todo.content}</p>
+                
+                {/* 因為todo的css，使用space-between排列，造成icon分散，所以在icon外包一層div解決此問題 */}
+                <div>
+                    {/* 修改icon */}
+                    <MdEdit onClick={() => { toggleIsEdit(todo.id) }} style={{ cursor: 'pointer' }} />
+                    {/* 刪除icon */}
+                    <MdDelete
+                        onClick={() => { deleteTodo(todo.id) }}
+                        style={{ cursor: 'pointer', marginLeft: '5px' }}
+                    />
+                </div>
+            </div>
     )
 }
+
 export default Todo
+
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -8,16 +8,67 @@ function TodoWrapper() {
     // 因為有N個todo，所以用陣列存放
     // const [todos, setTodos] = useState(['繳停車費', '對發票']);
     // 因為陣列需要key屬性，因此改為陣列物件
+    // const [todos, setTodos] = useState([
+    //     { content: '繳停車費', id: Math.random() },
+    //     { content: '對發票', id: Math.random() },
+    // ]);
+
+    // 因為要判定todo是否被點擊，所以要增加一個isCompleted屬性
+    // 因為要判定todo是否修改，所以要增加一個isEdit屬性
     const [todos, setTodos] = useState([
-        { content: '繳停車費', id: Math.random() },
-        { content: '對發票', id: Math.random() },
+        { content: '繳停車費', id: Math.random(), isCompleted: false, isEdit: false },
+        { content: '對發票', id: Math.random(), isCompleted: false, isEdit: false },
     ]);
 
     // 建立加入新的todo內容
     // 使用...其餘運算子保留原陣列內容
     // 再加上新的物件內容
     const addTodo = (content) => {
-        setTodos([...todos, { content: content, id: Math.random() }])
+        // setTodos([...todos, { content: content, id: Math.random() }])
+        // 這裡也要新增一個屬性
+        setTodos([...todos, { content: content, id: Math.random(), isCompleted: false, isEdit: false }])
+    }
+
+    // 建立刪除todo
+    // 傳入被刪除的id    
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => {
+            // 使用filter方法，保留不是被刪除的id
+            return todo.id !== id
+        }))
+    }
+
+    // 建立切換isCompleted屬性值函式，傳給Todo元件使用
+    const toggleCompleted = (id) => {
+        setTodos(todos.map((todo) => {
+            // 檢查被點擊的id是否跟陣列中的id一樣
+            // Yes => 1.取出todo 2.將isCompleted屬性反向(!=>Not)
+            // No  => todo不變
+            return todo.id === id
+                ? { ...todo, isCompleted: !todo.isCompleted }
+                : todo
+        }))
+    }
+
+    // 建立切換isEdit屬性值函式，傳給Todo元件使用
+    const toggleIsEdit = (id) => {
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+                ? { ...todo, isEdit: !todo.isEdit }
+                : todo
+        }))
+    }
+
+    // 建立完成修改的函式
+    // 修改完成會異動二個值
+    // 1. content 為新的修改內容
+    // 2. isEdit 改回 false
+    const editTodo = (id, newContent) => {
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+                ? { ...todo, content: newContent, isEdit: false }
+                : todo
+        }))
     }
 
     return (
@@ -28,7 +79,13 @@ function TodoWrapper() {
                 todos.map((todo) => {
                     // return <Todo />
                     // return <Todo todo={todo} />
-                    return <Todo todo={todo} key={todo.id} />
+                    // 增加deleteTodo屬性給Todo元件
+                    return <Todo todo={todo} key={todo.id}
+                        deleteTodo={deleteTodo}
+                        toggleCompleted={toggleCompleted}
+                        toggleIsEdit={toggleIsEdit}
+                        editTodo={editTodo}
+                    />
                 })
             }
         </div>
