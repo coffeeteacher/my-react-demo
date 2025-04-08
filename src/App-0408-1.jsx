fb5bc22950dcc306bbd84de50049e75b20cca8d8
coffeeteacher <coffeeteacher@gmail.com>
2025-04-08 16:45:00 +0800

    react-2

---
+++ src/App-0408-1.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1>Hi, React</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, laboriosam.</p>
+        </>
+    )
+}
+
+export default App;
