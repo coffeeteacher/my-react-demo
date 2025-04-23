d89946ef3598f16ff0e973ef16d52e80043bbdf6
coffeeteacher <coffeeteacher@gmail.com>
2025-04-23 10:22:10 +0800

    0423-todo

--- src/0422/components/Todo.jsx
+++ src/0422/components/Todo.jsx
@@ -1,4 +1,5 @@
 import { MdDelete, MdEdit } from "react-icons/md";
+import EditForm from "./EditForm";
 
 // 展示列表
 // 使用props接收元件屬性
@@ -11,22 +12,26 @@ import { MdDelete, MdEdit } from "react-icons/md";
 // }
 
 // 將props物件解構{元件屬性}
-function Todo({ todo, delTodo, toggleCompleted }) {
+function Todo({ todo, delTodo, toggleCompleted, toggleIsEdit, editTodo }) {
     return (
-        // 使用三元運算子
-        // 條件式?成立:不成立
-        <div className={`todo ${todo.isCompleted ? 'completed' : ''}`}>
-            <p onClick={() => { toggleCompleted(todo.id) }}>{todo.content}</p>
-            <div>
-                {/* 修改icon  */}
-                <MdEdit style={{ cursor: 'pointer' }} />
-                {/* 刪除icon  */}
-                <MdDelete
-                    onClick={() => { delTodo(todo.id) }}
-                    style={{ cursor: 'pointer', marginLeft: '5px' }}
-                />
+        todo.isEdit ? <EditForm todo={todo} editTodo={editTodo} /> :
+            // 使用三元運算子
+            // 條件式?成立:不成立
+            <div className={`todo ${todo.isCompleted ? 'completed' : ''}`}>
+                <p onClick={() => { toggleCompleted(todo.id) }}>{todo.content}</p>
+                <div>
+                    {/* 修改icon  */}
+                    <MdEdit
+                        onClick={()=>{toggleIsEdit(todo.id)}}
+                        style={{ cursor: 'pointer' }}
+                    />
+                    {/* 刪除icon  */}
+                    <MdDelete
+                        onClick={() => { delTodo(todo.id) }}
+                        style={{ cursor: 'pointer', marginLeft: '5px' }}
+                    />
+                </div>
             </div>
-        </div>
     )
 }
 
--- src/0422/components/TodoWrapper.jsx
+++ src/0422/components/TodoWrapper.jsx
@@ -5,14 +5,14 @@ import Todo from "./Todo"
 function TodoWrapper() {
     // 因為有N個todo，所以用陣列存放物件資料
     const [todos, setTodos] = useState([
-        { content: '停車費', id: Math.random(), isCompleted: false },
-        { content: '對發票', id: Math.random(), isCompleted: false },
+        { content: '停車費', id: Math.random(), isCompleted: false, isEdit: false },
+        { content: '對發票', id: Math.random(), isCompleted: false, isEdit: false },
     ])
 
     // 建立加入新的todo內容函式
     // 其餘運算子(...名稱)
     const addTodo = (newContent) => {
-        setTodos([...todos, { content: newContent, id: Math.random(), isCompleted: false }])
+        setTodos([...todos, { content: newContent, id: Math.random(), isCompleted: false, isEdit: false }])
     }
 
     // 建立刪除todo函式
@@ -37,6 +37,27 @@ function TodoWrapper() {
         }))
     }
 
+    // 建立切換isEdit屬性質函式，傳給Todo元件使用
+    const toggleIsEdit = (id) => {
+        setTodos(todos.map((todo) => {
+            // not false => true
+            // not true => false
+            return todo.id === id ? { ...todo, isEdit: !todo.isEdit } : todo
+        }))
+    }
+
+    // 建立完成修改的函式
+    // 修改完成後，會異動二個值
+    // content => 新的修改內容
+    // isEdit => 改回 false
+    const editTodo = (id, newContent) => {
+        setTodos(todos.map((todo) => {
+            return todo.id === id
+                ? { ...todo, content: newContent, isEdit: false }
+                : todo
+        }))
+    }
+
     return (
         <div className="wrapper">
             <h1>待辦事項</h1>
@@ -48,6 +69,8 @@ function TodoWrapper() {
                         key={todo.id}
                         delTodo={delTodo}
                         toggleCompleted={toggleCompleted}
+                        toggleIsEdit={toggleIsEdit}
+                        editTodo={editTodo}
                     />
                 })
             }
