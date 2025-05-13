35c9664c0f0bea4893680b4e43e5a6c3c0dc80fe
coffeeteacher <coffeeteacher@gmail.com>
2025-05-13 15:55:39 +0800

    0513-jquery

--- src/0513/App-jquery-scale.jsx
+++ src/0513/App-jquery-scale.jsx
@@ -1,25 +1,25 @@
-import { useEffect } from 'react';
-
+import { useEffect } from 'react'
 import $ from 'jquery'
-import './App.css';
+import './App.css'
 
 export default function App() {
-        
+
     useEffect(() => {
+        // 當滑鼠碰到圖片區域時，放大圖片
         $('.cssAnim1').on('mouseover', function () {
             $(this).addClass('imgScale');
         })
 
+        // 當滑鼠離開圖片區域時，還原圖片
         $('.cssAnim1').on('mouseout', function () {
             $(this).removeClass('imgScale');
         })
     }, [])
 
-
     return (
         <div>
             <a href="#" className="cssAnim1">
-                <img src="./images/p3.jpg" alt="" />
+                <img src="./images/coffee.jpg" alt="" />
             </a>
         </div>
     )
--- src/0513/App.jsx
+++ src/0513/App.jsx
@@ -1,9 +1,68 @@
-import './App.css'
-export default function App(){
-    return(
-        <>
-            <h1>jQuery</h1><br />
+import { useEffect } from 'react';
 
+import $ from 'jquery';
+import './App.css';
+
+export default function App() {
+
+    useEffect(() => {
+        // 移入 => mouseover()
+        // 移出 => mouseout()
+        // 移動 => mousemove()
+        // X、Y座標 => pageX、pageY
+        // 取得HTML內容 => html()
+        // 淡入 => fadeIn()
+        // 淡出 => fadeOut()
+        // 速度 => 毫秒 => (slow, normal, fast)
+        // 尋找指定的名稱 => has()
+        // 新增html元素 => append()
+        // 取得子元素 => children()
+
+        // 當滑鼠碰到超連結時，檢查「has()」是否有ttpShow類別
+        $('a:has(.ttpShow)')
+            .on('mouseover', function (e) {
+                // 動態增加「append()」一個div區域在body標籤中
+                // 取得超連結指定的子元素「children()」中的html內容「html()」
+                $('body').append('<div id="ttpPanel">' + $(this).children('.ttpShow').html() + '</div>');
+                $('#ttpPanel').css({
+                    top: (e.pageY + 10) + 'px',
+                    left: (e.pageX + 10) + 'px'
+                }).fadeIn('500');
+            })
+            .on('mouseout', function () {
+                $('#ttpPanel').remove();
+            })
+            .on('mousemove', function (e) {
+                $('#ttpPanel').css({
+                    top: (e.pageY + 10) + 'px',
+                    left: (e.pageX + 10) + 'px'
+                });
+            })
+    }, [])
+
+    return (
+        <>
+            <h2>ToolTip顯示說明</h2>
+            <p>
+                ToolTip浮動顯示文字
+                <a href='#'>
+                    顯示文字
+                    <span className='ttpShow'>consectetur內容說明...</span>
+                </a>
+            </p>
+
+            <p>
+                ToolTip浮動顯示圖片
+                <a href='#'>
+                    顯示圖片
+                    <span className='ttpShow'>
+                        <img src="./images/p3.jpg" alt="" style={{ width: '100px' }} />
+                        <p>
+                            Lorem ipsum dolor sit amet.
+                        </p>
+                    </span>
+                </a>
+            </p>
         </>
     )
 }
