544d5481620b3a433d866eef05b1886411cdbece
coffeeteacher <coffeeteacher@gmail.com>
2025-06-16 09:29:26 +0800

    0616

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import App from './0527-cart-ok/App.jsx'
+import App from './0421/App-0421-1.jsx'
 // import './index.css'
 // import './style.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
